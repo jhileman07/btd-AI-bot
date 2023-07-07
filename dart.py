1f61ccf5866f98e3774860c192492e60f0ea7d43
jhileman07 <johnhileman2026@u.northwestern.edu>
2023-07-07 01:52:25 -0500

    i think i can almost place a dart monkey

---
+++ dart.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+
+# Size of screen: 1440x900
+time.sleep(5)
+# Build dictionary mapping monkey names to coordinates
+locDict = {"dart": (908,864), "vscode": (1000,865), "slack": (1390,857)}
